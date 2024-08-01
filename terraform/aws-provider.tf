0017a6f801f12c19cf3fb6cd0d4b8c9fe9f4859d
saiprasannaorugonda789@gmail.com <sricharankaparthi123@gmail.com>
2024-08-01 12:12:18 +0530

    Backend

---
+++ terraform/aws-provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+
+   backend "s3" {
+    bucket = "terraform-practice15"
+    key    = "terraform-backendexpense-project"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+
+}
